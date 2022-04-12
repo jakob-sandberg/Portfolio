e7f35ba9d7bc347fe68e6cfa29ead8b862a3c077
Jakob <jsandberg_8@hotmail.com>
2022-04-12 11:32:31 +0200

    movie

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,6 +4,7 @@ import IMG1 from "../../assets/KRYPTO.png";
 import IMG2 from "../../assets/CLOTHING.png";
 import IMG3 from "../../assets/PHOTO.png";
 import IMG4 from "../../assets/DEVBOOK.png";
+import IMG5 from "../../assets/IMG5.png";
 
 const Portfolio = () => {
   const data = [
@@ -11,29 +12,35 @@ const Portfolio = () => {
       id: 1,
       image: IMG1,
       title: "Crypto Currency Tracker",
-      github: "https://github.com/",
+      github: "https://github.com/jakob-sandberg/CryptoMania/",
       demo: "https://crypto-manic.netlify.app/",
     },
     {
       id: 2,
       image: IMG2,
       title: " Clothing E-Commerce",
-      github: "https://github.com/",
+      github: "https://github.com/jakob-sandberg/jax-clothing",
       demo: "https://jax-clothing.netlify.app/",
     },
     {
       id: 3,
       image: IMG3,
       title: "Photo Review App",
-      github: "https://github.com/",
+      github: "https://github.com/jakob-sandberg/Foto-Review-app",
       demo: "https://foto-review.netlify.app",
     },
     {
       id: 4,
       image: IMG4,
       title: "Community For Developers",
-      github: "https://github.com/",
-      demo: "https://google.com/",
+      github: "https://github.com/jakob-sandberg/Devbook",
+      demo: "https://enigmatic-headland-43191.herokuapp.com/",
+    },
+    {
+      id: 5,
+      image: IMG5,
+      title: "My Own IMDB",
+      github: "https://bokaj-movie-hub.netlify.app/",
     },
   ];
   return (
@@ -50,7 +57,7 @@ const Portfolio = () => {
               </div>
               <h3>{title}</h3>
               <div className="portfolio__item-cta">
-                <a href={github} className="btn">
+                <a href={github} className="btn" target="_blank">
                   Github
                 </a>
                 <a href={demo} className="btn btn-primary" target="_blank">
